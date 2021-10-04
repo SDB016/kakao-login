d2b7f73542c947194b09f51f81d85c3d7e92f0f0
SDB016 <dongbin4013@naver.com>
2021-10-05 01:35:20 +0900

    feat: Change userdetail to User

--- src/main/java/kakaologin/auth/PrincipalDetail.java
+++ src/main/java/kakaologin/auth/PrincipalDetail.java
@@ -2,56 +2,17 @@ package kakaologin.auth;
 
 import kakaologin.domain.Member;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class PrincipalDetail implements UserDetails {
+public class PrincipalDetail extends User {
     private Member member;
 
-    public PrincipalDetail(Member member) {
+    public PrincipalDetail(Member member, Collection<? extends GrantedAuthority> authorities) {
+        super(member.getUserId(), member.getPassword(), authorities);
         this.member = member;
-    }
-
-    @Override
-    public String getPassword() {
-        return member.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return member.getUserId();
-    }
-
-    //계정이 만료되지 않았는지 리턴 (true: 만료 X)
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    //비밀번호가 만료되지 않았는지 리턴 (true: 만료 X)
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    //계정이 활성화(사용가능)인지 리턴
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    //계정의 권한 리턴
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collectors = new ArrayList<>();
-        collectors.add(() -> {return "ROLE_" + member.getRole();});
-        return collectors;
     }
 }
--- src/main/java/kakaologin/auth/PrincipalDetailService.java
+++ src/main/java/kakaologin/auth/PrincipalDetailService.java
@@ -5,11 +5,15 @@ import kakaologin.repository.MemberRepository;
 import kakaologin.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,6 +25,8 @@ public class PrincipalDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member principal = memberRepository.findByUserId(username).orElseThrow(() -> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다.: " + username));
-        return new PrincipalDetail(principal);
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(principal.getRole().name()));
+        return new PrincipalDetail(principal, roles);
     }
 }
